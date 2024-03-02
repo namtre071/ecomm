1b2ed4f653942e5433a9cfad7e7541e2c130af78
namtay071 <hongdiem31120@gmail.com>
2024-03-02 11:34:50 +0700

    product 1

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+    # thêm thuộc tính  image vào category, sau đó vào category controller thêm :image vào def cuối để có thể add ảnh vào category
     has_one_attached :image do |attachable|
         attachable.variant :thumb, resize_to_limit: [50,50]
     end
